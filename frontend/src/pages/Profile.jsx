e2dc1d0f3afe99d1f74ef0b09ed43e365de48435
Igli <iglibraho77@gmail.com>
2025-04-24 13:06:01 +0200

    fixes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import { Col, Container, Row, Image, Form, Button } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import '../index.css'
+import { assets } from '../assets/assets'
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user)
   return (
-    <div>Profile</div>
+    <Container fluid='md'>
+      <Row className='w-50 m-auto'>
+        <Col className='card'>
+          <Row>
+            <h1 className='text-center'>Profile</h1>
+            <Form >
+              <Col md={12}>
+                <Image src={assets.profile_img_1} width={64} alt='profile' className="d-block mx-auto mb-3"/>
+              </Col>
+              <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="firstname" name="firstname">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" name='firstName' />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="lastname" name="lastname">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" name='lastName' />
+                </Form.Group>
+              </Col>
+              </Row>
+              <Col md={12}>
+                <Form.Group className="mb-3" controlId="email" name="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" name='email' />
+                </Form.Group>
+              </Col>
+              <Col md={12}>
+                <Form.Group className="mb-3" controlId="password" name="password">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="password" name='password' />
+                </Form.Group>
+              </Col>
+              <Row>
+                <Col md={6}>
+                  <Button variant="danger" style={{ width: "100%", margin: "10px 0" }}>
+                    Delete Account
+                  </Button>
+                </Col>
+                <Col md={6}>
+                  <Button variant="danger" style={{ width: "100%", margin: "10px 0" }}>
+                    Sign out
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
